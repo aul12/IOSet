2df4d5dc335340b215044519f9452cc5610a3323
Paul Nykiel <paul.nykiel@gmail.com>
2020-05-18 22:42:10 +0200

    Refactoring

--- ioset.hpp
+++ ioset.hpp
@@ -8,28 +8,7 @@
 #define IOSET_IOSET_HPP
 
 #include "staticlist.hpp"
-
-template<typename T, typename List, T b, typename F, F f>
-struct ApplyOpVec {
-    using type = typename Prepend<T,
-            typename ApplyOpVec<T, typename List::next, b, F, f>::type, f(List::elem, b)>::type;
-};
-
-template<typename T, T b, typename F, F f>
-struct ApplyOpVec<T, ListEnd, b, F, f> {
-    using type = ListEnd;
-};
-
-template<typename List1, typename List2, typename F, F f>
-struct CartesianProduct {
-    using type = typename Concat<typename ApplyOpVec<typename List1::type, List2, List1::elem, F, f>::type,
-            typename CartesianProduct<typename List1::next, List2, F, f>::type>::type;
-};
-
-template<typename List2, typename F, F f>
-struct CartesianProduct<ListEnd, List2, F, f> {
-    using type = ListEnd;
-};
+#include "op.hpp"
 
 template<typename T, typename List, T t>
 struct Contains {
---
+++ op.hpp
@@ -0,0 +1,33 @@
+/**
+ * @file op.hpp
+ * @author paul
+ * @date 18.05.20
+ * Description here TODO
+ */
+#ifndef IOSET_OP_HPP
+#define IOSET_OP_HPP
+
+template<typename T, typename List, T b, typename F, F f>
+struct ApplyOpVec {
+    using type = typename Prepend<T,
+            typename ApplyOpVec<T, typename List::next, b, F, f>::type, f(List::elem, b)>::type;
+};
+
+template<typename T, T b, typename F, F f>
+struct ApplyOpVec<T, ListEnd, b, F, f> {
+    using type = ListEnd;
+};
+
+template<typename List1, typename List2, typename F, F f>
+struct CartesianProduct {
+    using type = typename Concat<typename ApplyOpVec<typename List1::type, List2, List1::elem, F, f>::type,
+            typename CartesianProduct<typename List1::next, List2, F, f>::type>::type;
+};
+
+template<typename List2, typename F, F f>
+struct CartesianProduct<ListEnd, List2, F, f> {
+    using type = ListEnd;
+};
+
+
+#endif //IOSET_OP_HPP
