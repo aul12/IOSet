2cdb4a9fed9380f70a9e6a31efb54ff329b87b35
Paul Nykiel <paul.nykiel@gmail.com>
2020-05-20 11:37:59 +0200

    Normalization for Operations

--- op.hpp
+++ op.hpp
@@ -8,6 +8,7 @@
 #define IOSET_OP_HPP
 
 #include "staticlist.hpp"
+#include "util.hpp"
 
 // Concepts for operations
 template<typename F, typename T>
@@ -18,17 +19,22 @@ concept binary_op = std::is_invocable<F, T, T>::value;
 
 // Transform a set with a unary operation
 template<static_list List, typename F, F f> requires unary_op<F, typename List::type>
-struct ApplyUnaryOp {
+struct AppleUnaryOpImpl {
     using type = typename Prepend<
                     typename List::type,
-                    typename ApplyUnaryOp<typename List::next, F, f>::type
+                    typename AppleUnaryOpImpl<typename List::next, F, f>::type
                     , f(List::elem)
                  >::type;
 };
 
 template<typename F, F f>
-struct ApplyUnaryOp<ListEnd, F, f> {
+struct AppleUnaryOpImpl<ListEnd, F, f> {
     using type = ListEnd;
+};
+
+template<static_list List, typename F, F f> requires unary_op<F, typename List::type>
+struct AppleUnaryOp {
+    using type = typename NormalizeList<typename AppleUnaryOpImpl<List, F, f>::type>::type;
 };
 
 // Helper function: Apply a binary operation with one fixed operand
@@ -49,17 +55,22 @@ struct ApplyBinaryOpVec<T, ListEnd, b, F, f> {
 // Calculate the cartesian product of a set over a given operation
 template<static_list List1, static_list List2, typename F, F f> requires same_list_type<List1, List2> &&
                                                                          binary_op<F, typename List2::type>
-struct CartesianProduct {
+struct CartesianProductImpl {
     using type = typename Concat<
                     typename ApplyBinaryOpVec<typename List1::type, List2, List1::elem, F, f>::type,
-                    typename CartesianProduct<typename List1::next, List2, F, f>::type
+                    typename CartesianProductImpl<typename List1::next, List2, F, f>::type
                 >::type;
 };
 
 template<static_list List2, typename F, F f>
-struct CartesianProduct<ListEnd, List2, F, f> {
+struct CartesianProductImpl<ListEnd, List2, F, f> {
     using type = ListEnd;
 };
 
+template<static_list List1, static_list List2, typename F, F f> requires same_list_type<List1, List2> &&
+                                                                         binary_op<F, typename List2::type>
+struct CartesianProduct {
+    using type = typename NormalizeList<typename CartesianProductImpl<List1, List2, F, f>::type>::type;
+};
 
-#endif //IOSET_OP_HPP
+#endif //IOSEtypename T_OP_HPP
